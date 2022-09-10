3bc0e5a579e7b7f39efff9ad429338c5c657d0be
Queen_J <jenniferogar300@gmail.com>
2022-09-10 15:29:58 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Description: prints lowercase alphabets
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 
 	int ch;
+
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 
 		if (ch != 'e' && ch != 'q')
 		{								
-			putchart(ch);
+			putchar(ch);
 		}
 		else
 		{												
