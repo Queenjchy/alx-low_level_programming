cd89b245d301d37d411edf94ac7723d2612559c5
Queen_J <jenniferogar300@gmail.com>
2022-09-15 13:41:38 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,9 @@ void print_alphabet_x10(void)
 	int i = 0;
 
 	while (i <= 9)
-	{
-        	for (c = 'a'; c <= 'z'; c++)
+	{i
+                for (c = 'a'; c <= 'z'; c++)
+
 		{
 			_putchar(c);
 		}
