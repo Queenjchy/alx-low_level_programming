b2f202b371a9c979c54ff57a781dd19b2c4c9003
Queen_J <jenniferogar300@gmail.com>
2022-09-19 05:44:52 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int -putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,3 +16,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif /* MAIN_H */
