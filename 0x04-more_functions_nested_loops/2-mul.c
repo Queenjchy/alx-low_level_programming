0bd52bb995aacf1fbed304c563b2979d17f34fa7
Queen_J <jenniferogar300@gmail.com>
2022-09-19 10:12:09 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- *
- * @a: first input
- * @b: second input
- *
- * Return: return results
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
  */
 
 int mul(int a, int b)
+
 {
-	return (a * b);
+
+		return (a * b);
+
 }
