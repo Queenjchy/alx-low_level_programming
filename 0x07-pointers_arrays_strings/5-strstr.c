0132a0a131b7026bc3d5840969285850757ede57
Queen_J <jenniferogar300@gmail.com>
2022-09-28 15:06:06 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -30,8 +30,10 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[index + 1] == '\0')
-				return (haystack);	 
-				index++;			
+					return (haystack);
+
+				index++;
+
 			} while (haystack[index] == needle[index]);
 		}
 
